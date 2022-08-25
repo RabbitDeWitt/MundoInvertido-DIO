0b29c15053eaa62e2832bf0889838faf954473eb
David Coelho <davidsocoelho@outlook.com>
2022-08-25 20:14:46 -0300

    Bugs resolvidos

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,9 +1,6 @@
-
 import { subscribeToHellfireClube } from "./firebase/hellfire-clube.js"
 
-const audio = document.getElementById('music')
 const btnSubscribe = document.getElementById('btnSubscribe')
-audio.volume = 0.1
 
 btnSubscribe.addEventListener('click', async() =>{
   const subscrition = {
@@ -15,15 +12,3 @@ btnSubscribe.addEventListener('click', async() =>{
 
   await subscribeToHellfireClube(subscrition)
 })
-
-function switchTheme(){
-  document.body.classList.toggle('light-theme')
-  document.body.classList.toggle('dark-theme')
-
-  const theme = document.body.className;
-
-  const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
-  audio.src = `assets/musics/${music}`
-  audio.play()
-  audio.volume = 0.1
-}
---
+++ assets/JS/theme.js
@@ -0,0 +1,16 @@
+const audio = document.getElementById('music')
+audio.volume = 0.1
+audio.play()
+
+function switchTheme(){
+  document.body.classList.toggle('light-theme')
+  document.body.classList.toggle('dark-theme')
+
+
+  const theme = document.body.className;
+
+  const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+  audio.src = `assets/musics/${music}`
+  audio.play()
+  audio.volume = 0.1
+}
